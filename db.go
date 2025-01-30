df4f1582f7a8b163a75cbd2ac64f5fbae02242db
dr0zd <maximdrozdov29@yandex.ru>
2025-01-31 03:17:17 +0700

    write base for BD from 2-nd lesson

--- db.go
+++ db.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+)
+
+// переменная через которую мы будем работать с БД
+var DB *gorm.DB
+
+func InitDB() {
+	//в dsn вводим данные, которые мы указали при создании контейнера
+	dsn := "host=localhost user=postgres password=bebra dbname=postgres port=5432"
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database: ", err)
+	}
+}
