b994e1659c1359796deeef7de2e7b8d4708d2b71
dr0zdlaptop <maximdrozdov29@yandex.ru>
2025-02-14 16:19:16 +0700

    remove automigrate

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -12,7 +12,7 @@ import (
 
 func main() {
 	database.InitDB()
-	database.DB.AutoMigrate(&taskService.Task{})
+	//database.DB.AutoMigrate(&taskService.Task{})
 
 	repo := taskService.NewTaskRepository(database.DB)
 	service := taskService.NewService(repo)
