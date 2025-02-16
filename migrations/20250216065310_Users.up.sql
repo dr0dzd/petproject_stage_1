45cb59b0fc07f6ff085d1542494ab7ad59acf6aa
dr0zdlaptop <maximdrozdov29@yandex.ru>
2025-02-16 14:33:12 +0700

    up migrate for users

---
+++ migrations/20250216065310_Users.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Users (
+    id    SERIAL PRIMARY KEY,
+    email TEXT NOT NULL UNIQUE CHECK (email ~* '^[A-Za-z0-9._]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$') ,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    deleted_at TIMESTAMP DEFAULT NULL
+);
