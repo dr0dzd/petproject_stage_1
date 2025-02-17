90d65b16c09551decb3015f9ae65242e3b3a37f5
dr0zdlaptop <maximdrozdov29@yandex.ru>
2025-02-18 03:02:35 +0700

    add strictUserHandler and second table in DB (Named Users)

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,7 +4,9 @@ import (
 	"Golang/internal/database"
 	"Golang/internal/handlers"
 	"Golang/internal/taskService"
+	"Golang/internal/userService"
 	"Golang/internal/web/tasks"
+	"Golang/internal/web/users"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"log"
@@ -14,18 +16,23 @@ func main() {
 	database.InitDB()
 	//database.DB.AutoMigrate(&taskService.Task{})
 
-	repo := taskService.NewTaskRepository(database.DB)
-	service := taskService.NewService(repo)
+	taskRepo := taskService.NewTaskRepository(database.DB)
+	taskServ := taskService.NewService(taskRepo)
+	taskHandler := handlers.NewTaskHandler(taskServ)
 
-	handler := handlers.NewHandler(service)
+	userRepo := userService.NewUserRepository(database.DB)
+	userServ := userService.NewUserService(userRepo)
+	userHandler := handlers.NewUserHandler(userServ)
 
 	e := echo.New()
-
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	strictHandler := tasks.NewStrictHandler(handler, nil)
-	tasks.RegisterHandlers(e, strictHandler)
+	strictTaskHandler := tasks.NewStrictHandler(taskHandler, nil)
+	tasks.RegisterHandlers(e, strictTaskHandler)
+
+	strictUserHandler := users.NewStrictHandler(userHandler, nil)
+	users.RegisterHandlers(e, strictUserHandler)
 
 	if err := e.Start("localhost:8080"); err != nil {
 		log.Fatalf("failed to start with err: %v", err)
