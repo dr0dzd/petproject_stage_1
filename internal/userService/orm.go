df9e4e33b5befac29c96e6be2fd70cfdc19c68cf
dr0zdlaptop <maximdrozdov29@yandex.ru>
2025-02-21 16:32:49 +0700

    added gorm tags for connect users and tasks

--- internal/userService/orm.go
+++ internal/userService/orm.go
@@ -1,9 +1,13 @@
 package userService
 
-import "gorm.io/gorm"
+import (
+	"Golang/internal/taskService"
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email    string             `json:"email" gorm:"unique;not null"`
+	Password string             `json:"password"`
+	Tasks    []taskService.Task `json:"tasks,omitempty" gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"`
 }
